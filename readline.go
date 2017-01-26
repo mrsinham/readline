09eeff7f26264df8b05a7bd31d84fa768beebb6f
Julien Lefevre <julien.lefevre@twenga.com>
2017-01-26 18:10:23 +0100

    Using a fillable stdin reader

--- readline.go
+++ readline.go
@@ -57,9 +57,10 @@ type Config struct {
 
 	FuncGetWidth func() int
 
-	Stdin  io.Reader
-	Stdout io.Writer
-	Stderr io.Writer
+	Stdin       io.Reader
+	StdinWriter io.Writer
+	Stdout      io.Writer
+	Stderr      io.Writer
 
 	EnableMask bool
 	MaskRune   rune
@@ -100,6 +101,9 @@ func (c *Config) Init() error {
 	if c.Stdin == nil {
 		c.Stdin = NewCancelableStdin(Stdin)
 	}
+
+	c.Stdin, c.StdinWriter = NewFillableStdin(c.Stdin)
+
 	if c.Stdout == nil {
 		c.Stdout = Stdout
 	}
